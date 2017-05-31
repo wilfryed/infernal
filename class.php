41df37c4fd7d0a1cf05e840eb3c8d6fa7c15dbbb
wilfryed <wilfryed@hotmail.com>
2017-05-31 13:08:31 +0200

    Ajout des templates

--- class.php
+++ class.php
@@ -1,6 +1,44 @@
 <?php
 
 class Infernal {
+<<<<<<< HEAD
+
+    private $_content;
+    private $_js;
+
+    public function __construct(){
+        $_content = '';
+        $_js = '';
+    }
+
+    public function getFooter(){
+        $return = file_get_contents('templates/footer.html', FILE_USE_INCLUDE_PATH);
+        $return = str_replace("%%%JS%%%", $this->_js, $return);
+        $this->_content .= $return;
+    }
+
+    public function getHeader(){
+        $this->_content = file_get_contents('templates/header.html', FILE_USE_INCLUDE_PATH);
+    }
+
+    public function getTemplatePart($part, $articles){
+        include('templates/'.$part.'.html');
+    }
+
+    public function loadCss(){
+
+    }
+
+    public function loadJs($script){
+        $return = '<script src="'.$script.'"></script>'.PHP_EOL;
+        $this->_js .= $return;
+    }
+
+    public function display(){
+        echo $this->_content;
+        $this->_content = '';
+    }
+=======
     
     private $_content;
     
@@ -39,12 +77,16 @@ class Infernal {
         echo $this->_content;
         $this->_content = '';
     }
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
 }
 
 class Articles {
 
     private $path;
     private $maxItems;
+    private $currentPage;
+    private $currentIndex;
+    private $currentEntry;
 
     public function __construct($path = true, $maxItems = null) {
         if ($path === true) {
@@ -54,6 +96,9 @@ class Articles {
         }
 
         $this->maxItems = $maxItems;
+        $this->currentPage = 1;
+        $this->currentIndex = false;
+        $this->currentEntry = false;
     }
 
     private function getData() {
@@ -85,12 +130,30 @@ class Articles {
 
     private function countItems() {
         $i = 0;
+<<<<<<< HEAD
+        if ($this->currentIndex){
+            foreach (glob($this->path.'/'.$this->currentIndex.'.txt') as $filename) {
+                $lines = file($filename);
+                $lines = array_values(array_filter($lines, "trim"));
+                foreach ($lines as $line) {
+                    $i++;
+                }
+=======
         foreach (glob($this->path) as $filename) {
             $lines = file($filename);
             $lines = array_values(array_filter($lines, "trim"));
             foreach ($lines as $line) {
                 $i++;
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
             }
+        }else{
+            foreach (glob($this->path) as $filename) {
+                $lines = file($filename);
+                $lines = array_values(array_filter($lines, "trim"));
+                foreach ($lines as $line) {
+                    $i++;
+                }
+            } 
         }
 
         return $i;
@@ -115,26 +178,30 @@ class Articles {
         return $item[0];
     }
 
+<<<<<<< HEAD
+    public function setCurrentPage($page) {
+        $this->currentPage = $page;
+    }
+
+    public function setCurrentIndex($index) {
+        $this->currentIndex = $index;
+    }
+
+    public function setCurrentEntry($entry) {
+        $this->currentEntry = $entry;
+    }
+
+=======
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
     public function getContent() {
         return $this->content;
     }
 
-    public function displayArticle($title) {
-        $data = $this->getData();
-        $return = '';
-
-        foreach ($data as $parts) {
-            foreach ($parts as $item) {
-                if ($this->sanitize($this->itemLink($item)) == $title) {
-                    $return .= '<p>' . $this->replace($item) . '</p>';
-                }
-            }
-        }
-
-        return $return;
-    }
-
+<<<<<<< HEAD
     public function displayPage($page = 1, $maxItems = null) {
+=======
+    public function displayArticle($title) {
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
         $data = $this->getData();
         $return = '';
         $i = 0;
@@ -155,9 +222,76 @@ class Articles {
             $return .= '<p><a href="http://' . $_SERVER['SERVER_NAME'] . '/app/infernal/entry/' . $this->sanitize($this->itemLink($item)) . '">Lire la suite</a></p>';
         }
 
+<<<<<<< HEAD
         return $return;
     }
 
+    public function getEntry() {
+        $data = $this->getData();
+        $return = '';
+        if ($this->currentEntry){
+            foreach ($data as $parts) {
+                foreach ($parts as $item) {
+                    if ($this->sanitize($this->itemLink($item)) == $this->currentEntry) {
+                        $return .= '<p>' . $this->replace($item) . '</p>';
+                    }
+=======
+        foreach ($data as $parts) {
+            foreach ($parts as $item) {
+                if ($this->sanitize($this->itemLink($item)) == $title) {
+                    $return .= '<p>' . $this->replace($item) . '</p>';
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
+                }
+            }
+        }
+
+        return $return;
+    }
+
+<<<<<<< HEAD
+    public function getEntries($maxItems = null) {
+=======
+    public function displayPage($page = 1, $maxItems = null) {
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
+        $data = $this->getData();
+        $return = '';
+        $i = 0;
+        if ($maxItems == null) {
+            $maxItems = $this->maxItems;
+        }
+<<<<<<< HEAD
+        if ($this->currentIndex){
+            $items = $data['contents/' . $this->currentIndex];
+        }else{
+            foreach ($data as $parts) {
+                foreach ($parts as $item) {
+                    $items[] = $item;
+                }
+            }
+        }
+
+        $items = new LimitIterator(new ArrayIterator($items), ($this->currentPage * $maxItems) - $maxItems, $maxItems * $this->currentPage);
+=======
+
+        foreach ($data as $parts) {
+            foreach ($parts as $item) {
+                $items[] = $item;
+            }
+        }
+
+        $items = new LimitIterator(new ArrayIterator($items), ($page * $maxItems) - $maxItems, $maxItems * $page);
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
+
+        foreach ($items as $item) {
+            $return .= '<p>' . substr($this->replace($item, true), 0, 100) . '...</p>';
+            $return .= '<p><a href="http://' . $_SERVER['SERVER_NAME'] . '/app/infernal/entry/' . $this->sanitize($this->itemLink($item)) . '">Lire la suite</a></p>';
+        }
+
+        return $return;
+    }
+
+<<<<<<< HEAD
+=======
     public function displayIndex($index) {
         $data = $this->getData();
         $data = $data['contents/' . $index];
@@ -171,6 +305,7 @@ class Articles {
         return $return;
     }
 
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
     public function pagination($maxItems = null) {
         if ($maxItems == null) {
             $maxItems = $this->maxItems;
@@ -247,6 +382,23 @@ class Articles {
     private function sanitize($texte) {
         $texte = mb_strtolower($texte, 'UTF-8');
         $texte = str_replace(
+<<<<<<< HEAD
+            array(
+                'à', 'â', 'ä', 'á', 'ã', 'å',
+                'î', 'ï', 'ì', 'í',
+                'ô', 'ö', 'ò', 'ó', 'õ', 'ø',
+                'ù', 'û', 'ü', 'ú',
+                'é', 'è', 'ê', 'ë',
+                'ç', 'ÿ', 'ñ',
+            ), array(
+                'a', 'a', 'a', 'a', 'a', 'a',
+                'i', 'i', 'i', 'i',
+                'o', 'o', 'o', 'o', 'o', 'o',
+                'u', 'u', 'u', 'u',
+                'e', 'e', 'e', 'e',
+                'c', 'y', 'n',
+            ), $texte
+=======
                 array(
             'à', 'â', 'ä', 'á', 'ã', 'å',
             'î', 'ï', 'ì', 'í',
@@ -262,11 +414,16 @@ class Articles {
             'e', 'e', 'e', 'e',
             'c', 'y', 'n',
                 ), $texte
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
         );
 
         return $texte;
     }
+<<<<<<< HEAD
+
+=======
     
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
     public function getEntryTitle(){
         $data = $this->getData();
         $i=0;
--- index.php
+++ index.php
@@ -9,11 +9,33 @@ $infernal = new Infernal();
 $infernal->getHeader();
 $infernal->loadCss();
 $infernal->display();
+<<<<<<< HEAD
+=======
 
 if (isset($_GET['entry'])) {
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
 
+if (isset($_GET['entry'])) {
+    //entry
+    
     $index = substr($_GET['entry'], 0, 1);
+    
+    $articles = new articles();
+    $articles->setCurrentEntry($_GET['entry']);
+    $infernal->getTemplatePart('entry', $articles);
+    
+} elseif (isset($_GET['page'])) {
+    // page
+    
+    $articles = new articles(true, "4");
+    $articles->setCurrentPage($_GET['page']);
+    $infernal->getTemplatePart('homepage', $articles);
 
+<<<<<<< HEAD
+} elseif ((isset($_GET['index']))) {    
+    //index
+    
+=======
     $articles = new articles();
 
     echo $articles->displayArticle($_GET['entry']);
@@ -21,8 +43,28 @@ if (isset($_GET['entry'])) {
     echo $articles->get_nextentry($_GET['entry']);
 } elseif (isset($_GET['page'])) {
 
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
     $articles = new articles(true, "4");
+    $articles->setCurrentPage(1);
+    $articles->setCurrentIndex($_GET['index']);
+    $infernal->getTemplatePart('homepage', $articles);
 
+<<<<<<< HEAD
+} else {
+    //homepage
+
+    $articles = new articles(true, "4");
+    $infernal->getTemplatePart('homepage', $articles);
+
+}
+
+echo '<script>';
+echo "var entries = [";
+$items = $articles->getEntryTitle();
+foreach ($items as $item){
+    $item = explode('#',$item);
+    echo "{'slug': '".$item[1]."', 'value': '".$item[0]."' }, ";
+=======
     echo $articles->menu();
     echo $articles->displayPage($_GET['page']);
     echo $articles->pagination();
@@ -43,8 +85,15 @@ if (isset($_GET['entry'])) {
     echo $articles->menu();
     echo $articles->displayPage();
     echo $articles->pagination();
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
 }
+echo "];";
+echo '</script>';
 
+<<<<<<< HEAD
+$infernal->loadJs('http://wilfryed.com/app/infernal/assets/js/vendor/typeahead.bundle.js');
+$infernal->loadJs('http://wilfryed.com/app/infernal/assets/js/app.js');
+=======
 echo '<script>';
 echo "var entries = [";
 $items = $articles->getEntryTitle();
@@ -58,6 +107,7 @@ echo '</script>';
 $infernal->loadJs('assets/js/vendor/jquery.js');
 $infernal->loadJs('assets/js/vendor/typeahead.bundle.js');
 $infernal->loadJs('assets/js/app.js');
+>>>>>>> bd7ff7642dd0bcb64437354d475bada611a0078f
 $infernal->getFooter();
 $infernal->display();
 ?>
